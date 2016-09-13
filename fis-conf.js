b2b4c60ea233ba31db2183868fe718c52672c630
lidianhao123 <dainli@outlook.com>
2016-09-13 14:16:43 +0800

    第一次修改完成

--- fis-conf.js
+++ fis-conf.js
@@ -1,63 +1,62 @@
-// fis.match('::package', {
-//   postpackager: fis.plugin('loader', {
-//     allInOne:true
-//   })
-// });.
-// http://fis.baidu.com/fis3/docs/api/config-props.html
-fis.set('project.ignore', ['fis-conf.js','config.json', '.gitignore', 'node_modules/**', 'output/**',]);
-// 启用 fis-spriter-csssprites 插件
-fis.match('::package', {
-  spriter: fis.plugin('csssprites')
-});
+/*
+ * @auth lidian
+ * @date 2016-05-16
+ * @desc 
+ * 全局安装 fis3: sudo npm install -g fis3
+ * 安装less插件 : sudo npm install -g fis-parser-less
+ * 开发命令     : fis3 server start //开启fis3 本地server
+ *                fis3 server open  //打开fis3 本地server文件夹路径
+ *                fis3 server clean //清空本地server文件夹路径内容
+ *                fis3 release debug -wL //将当前路径代码发布到本地调试服务器并且监听文件修改自动刷新浏览器
+ *                fis3 release prod //发布代码到当前路径\output下
+ * 
+ * FIS3 构建会对 CSS 中，路径带 ?__sprite 的图片进行合并
+ * http://fis.baidu.com/fis3/docs/beginning/release.html#CssSprite%E5%9B%BE%E7%89%87%E5%90%88%E5%B9%B6
+ */
+//FIS3 官方文档 http://fis.baidu.com/fis3/docs/beginning/intro.html
+//全局属性文档 http://fis.baidu.com/fis3/docs/api/config-props.html
+fis.set('project.ignore', ['printImgNames.js','fis-conf.js','node_modules/**', 'output/**',]);
 
-//发布到自己服务器wmb下面
-var selfWmbPath = "/wmb";
-fis.media("self").match('(*.{png,gif,jpg})', {
-  url:selfWmbPath + "$0"
-});
-fis.media("self").match("(*.less)", {
-  parser: fis.plugin('less'),
-  url:selfWmbPath + "$0",
-  optimizer: fis.plugin('clean-css'),
-  rExt: '.css'
-});
-fis.media("self").match("(*.css)", {
-  url:selfWmbPath + "$0",
-  optimizer: fis.plugin('clean-css')
-});
-fis.media("self").match("(*.js)", {
-  url:selfWmbPath + "$0"
-});
-fis.media("self").match("(*.mp3)", {
-  url:selfWmbPath + "$0"
-});
+fis.hook('relative');
 
-//发布到php环境下资源路径
-var prodPath = "/Public";
-fis.media("prod").match('(*.{png,gif,jpg})', {
-  url:prodPath+"$0"
-});
+//发布到默认的当前路径output下
+fis.media("prod")
+  .match('::package', {
+    spriter: fis.plugin('csssprites')
+  })
+  .match("(*.less)", {
+    parser: fis.plugin('less-2.x'),
+    rExt: '.css',
+    // 开发阶段默认不开启csssprites，如有需要设置成true即可
+    useSprite: false,
+    optimizer: fis.plugin('clean-css'),
+    postprocessor: fis.plugin('autoprefixer',{
+      "browsers": ["Android >= 2.3", "ChromeAndroid > 1%", "iOS >= 4"],
+      "cascade": true
+    })
+  })
+  .match('**', {
+    relative: true
+  })
+  //将代码直接部署至./output下
+  .match('*', {
+    deploy: fis.plugin('local-deliver', {
+      to: './output'
+    })
+  });
 
-fis.media("prod").match("(*.less)", {
-  parser: fis.plugin('less'),
-  url:prodPath+"$0",
-  optimizer: fis.plugin('clean-css'),
-  rExt: '.css'
-});
-fis.media("prod").match("(*.css)", {
-  url:prodPath+"$0",
-  optimizer: fis.plugin('clean-css')
-});
-fis.media("prod").match("(*.js)", {
-  url:prodPath+"$0"
-});
-fis.media("prod").match("(*.mp3)", {
-  url:selfWmbPath + "$0"
-});
-
-fis.media("debug").match("(*.less)", {
-  parser: fis.plugin('less'),
-  // .less 文件后缀构建后被改成 .css 文件
-  // useSprite: true, ?__sprite
-  rExt: '.css'
-});
+//开发阶段使用
+fis.media("debug")
+  .match('::package', {
+    spriter: fis.plugin('csssprites')
+  })
+  .match("(*.less)", {
+    parser: fis.plugin('less-2.x'),
+    rExt: '.css',
+    // 开发阶段默认不开启csssprites，如有需要设置成true即可
+    useSprite: false,
+    postprocessor: fis.plugin('autoprefixer',{
+      "browsers": ["Android >= 2.3", "ChromeAndroid > 1%", "iOS >= 4"],
+      "cascade": true
+    })
+  });
--- js/pullload.js
+++ js/pullload.js
@@ -1,117 +1,332 @@
-define("pullload", ["zepto"], function(zepto) {
+// define("pullload", ["zepto"], function(zepto) {
 
-  var pullload = function(options) {
-    this.defaultConfig = {
-      container: zepto(document.body),
-      el: zepto(document.body),          
-      offsetScrollTop: 2,
-      offsetY: 75,
-      distanceBottom: 30,
-      isbody:true,
-      onPullDownMove:function(){},
-      onPullDownRefresh:function(){},
-      clearPullDownMove:function(){},
-      onPullStart:function(){},
-      onPullUpMove:function(){},
-      onPullUpLoad:function(){}
-    };
-    this.config = zepto.extend(this.defaultConfig, options || {});
-    this.init.call(this);
+(function(){
+
+  coreDefaultConfig = {       
+    offsetScrollTop: 2,
+    offsetY: 75,
+    distanceBottom: 100,
+    onPullDownMove:function(){},
+    onPullDownRefresh:function(){},
+    clearPullDownMove:function(){},
+    onPullUpMove:function(){},
+    onPullUpLoad:function(){}
+  }
+
+  var pullloadCore = function(opts) {
+    this.init(opts);
   };
 
-  zepto.extend(pullload.prototype, {
-    init: function() {
-      this._cacheDom();
-      this._initEvent();
-      this.config.isbody = this.config.container[0] === document.body;
-      console.info(this.config.isbody);
-    },
-    _cacheDom: function() {
-      this.el = "string" == typeof this.config.el ? zepto(this.config.el) : this.config.el
-    },
-    _initEvent: function() {
-      var self = this,                 //当前对象，闭包处理
-        config = this.config,          //当前对象的配置参数
-        element = this.el,             //根元素，zepto对象
-        startX = 0,                    //用于保存touchstart时初始位置
-        startY = 0;                    //用于保存touchstart时初始位置
+  pullloadCore.prototype = {
+    //用于保存touchstart时初始位置
+    startX: 0,
+    //用于保存touchstart时初始位置
+    startY: 0,
+    //配置信息
+    config: null,
 
-      //绑定touchstart
-      element.on("touchstart", function(event) {
-        var targetEvent = event.changedTouches[0];
-        startX = targetEvent.clientX;
-        startY = targetEvent.clientY;
-        config.onPullStart();
-      });
-      //绑定touchmove
-      element.on("touchmove", function(event) {
-        var scrollTop = config.container.scrollTop(),
-          scrollH = config.container.prop("scrollHeight"),
-          con_H = config.isbody ? document.documentElement.clientHeight : config.container.height(),
-          targetEvent = event.changedTouches[0],
-          curX = targetEvent.clientX,
-          curY = targetEvent.clientY,
-          diffX = curX - startX,
-          diffY = curY - startY;
-          console.info(diffY);
-console.info(scrollH, scrollTop,con_H);
-        //判断垂直移动距离是否大于5 && 横向移动距离小于纵向移动距离
-        if(Math.abs(diffY) > 5 && Math.abs(diffY) > Math.abs(diffX)){
-          //滚动距离小于设定值 &&回调onPullDownMove 函数，并且回传位置值
-          if(diffY > 5 && scrollTop < config.offsetScrollTop ){
-            //阻止执行浏览器默认动作
-            event.preventDefault();
-            config.onPullDownMove([{
-              touchStartY: startY,
-              touchMoveY: curY
-            }]);
-          } //滚动距离距离底部小于设定值
-          else if(diffY < 0 && (scrollH - scrollTop - con_H) < config.distanceBottom ){
-            //阻止执行浏览器默认动作
-            event.preventDefault();
-            config.onPullUpMove([{
-              touchStartY: startY,
-              touchMoveY: curY
-            }]);
-          }
-        }
-      });
-      //绑定touchend
-      element.on("touchend", function(event) {
-        var scrollTop = config.container.scrollTop(),
-          scrollH = config.container.prop("scrollHeight"),
-          con_H = config.isbody ? document.documentElement.clientHeight : config.container.height(),
-          targetEvent = event.changedTouches[0],
-          curX = targetEvent.clientX,
-          curY = targetEvent.clientY,
-          diffX = curX - startX,
-          diffY = curY - startY;
+    init: function(opts){
+      this.config = extend(coreDefaultConfig, opts || {});
+    },
+    onStart: function(x, y){
+      this.startX = x;
+      this.startY = y;
+    },
+    onMove: function(x, y, scrollTop, scrollH, conH){
+      var diffX = x - this.startX,
+        diffY = y - this.startY;
 
-        //判断垂直移动距离是否大于5 && 横向移动距离小于纵向移动距离
-        if(Math.abs(diffY) > 5 && Math.abs(diffY) > Math.abs(diffX)){
-          if(diffY > 5 && scrollTop < config.offsetScrollTop ){
-            //回调onPullDownRefresh 函数，即满足刷新条件
-            config.onPullDownRefresh();
-          }
-          else if(diffY < 0 && (scrollH - scrollTop - con_H) < config.distanceBottom ){
-            //回调onPullUpLoad 函数，即满足刷新条件
-            config.onPullUpLoad();
-          }
-          else{
-            //回调clearPullDownMove 函数，取消刷新动作
-            config.clearPullDownMove();
-          }
+      //判断垂直移动距离是否大于5 && 横向移动距离小于纵向移动距离
+      if(Math.abs(diffY) > 5 && Math.abs(diffY) > Math.abs(diffX)){
+        //滚动距离小于设定值 &&回调onPullDownMove 函数，并且回传位置值
+        if(diffY > 5 && scrollTop < this.config.offsetScrollTop ){
+          // //阻止执行浏览器默认动作
+          // event.preventDefault();
+          this.config.onPullDownMove(this.startY, y);
+        } //滚动距离距离底部小于设定值
+        else if(diffY < 0 && (scrollH - scrollTop - conH) < this.config.distanceBottom ){
+          //阻止执行浏览器默认动作
+          // event.preventDefault();
+          this.config.onPullUpMove(this.startY, y);
         }
-        // //判断垂直移动距离是否大于设定值 && 容器滚动距离小于设定值 && 横向移动距离小于纵向移动距离
-        // if( diffY > config.offsetY && s < config.offsetScrollTop && Math.abs(diffX) < Math.abs(diffY)){
-        //   //回调onPullDownRefresh 函数，即满足刷新条件
-        //   config.onPullDownRefresh();
-        // } else {
-        //   //回调clearPullDownMove 函数，取消刷新动作
-        //   config.clearPullDownMove();
+      }
+    },
+    onEnd: function(x, y, scrollTop, scrollH, conH){
+       var diffX = x - this.startX,
+        diffY = y - this.startY;
+
+      //判断垂直移动距离是否大于5 && 横向移动距离小于纵向移动距离
+      if(Math.abs(diffY) > 5 && Math.abs(diffY) > Math.abs(diffX)){
+        if(diffY > 5 && scrollTop < this.config.offsetScrollTop ){
+          //回调onPullDownRefresh 函数，即满足刷新条件
+          this.config.onPullDownRefresh();
+        }
+        // else if(diffY < 0 && (scrollH - scrollTop - conH) < this.config.distanceBottom ){
+        //   //回调onPullUpLoad 函数，即满足刷新条件
+        //   this.config.onPullUpLoad();
         // }
-      });
+        else{
+          //回调clearPullDownMove 函数，取消刷新动作
+          this.config.clearPullDownMove();
+        }
+      }
     }
-  });
-  return pullload;
-});
+  }
+
+  defaultConfig = {
+    container: null,   //具有scroll的容器
+    wrapper: null,     //结构外包围元素
+    // offsetScrollTop: 2,
+    // offsetY: 75,
+    // distanceBottom: 30,
+    onRefresh: function(){},
+    onLoadMore: function(){}
+  }
+  extendFns = ["onPullDownMove", "onPullDownRefresh", "clearPullDownMove", "onPullUpMove", "onPullUpLoad"];
+  extendProps = ["offsetScrollTop", "offsetY", "distanceBottom"];
+
+  var STATS = {
+    init: '',
+    pulling: 'pulling',
+    enough: 'pulling enough',
+    refreshing: 'refreshing',
+    refreshed: 'refreshed',
+    reset: 'reset',
+
+    loading: 'loading' // loading more
+  };
+
+  var pullload = function(opts){
+    this.init(opts);
+  }
+
+  pullload.prototype = {
+    config: {},
+    container: null,   //具有scroll的容器
+    wrapper: null,     //结构外包围元素
+    loaderBody: null,  //DOM 对象
+    loaderSymbol: null, //DOM 对象
+    loaderBtn: null,    //DOM 对象
+    _core: null,       //pullloadCore 实例
+    loaderState: STATS.init,
+    hasMore: true,    //是否有加载更多
+    init: function(opts){
+      this.config.onRefresh = opts.onRefresh || function(){};
+      this.config.onLoadMore = opts.onLoadMore || function(){};
+      this.container = opts.container;
+      this.wrapper = opts.wrapper;
+      this.loaderBody = this.wrapper.querySelector(".tloader-body");
+      this.loaderSymbol = this.wrapper.querySelector(".tloader-symbol");
+      this.loaderBtn = this.wrapper.querySelector(".tloader-btn");
+      // var coreOpts = extendArrProps(extendFns, {}, this);
+      var coreOpts = {
+        onPullDownMove: bind(this.onPullDownMove, this),
+        onPullDownRefresh: bind(this.onPullDownRefresh, this),
+        clearPullDownMove: bind(this.clearPullDownMove, this),
+        onPullUpMove: bind(this.onPullUpMove, this),
+        onPullUpLoad: bind(this.onPullUpLoad, this)
+      }
+      coreOpts = extendArrProps(extendProps, coreOpts, opts);
+      this._core = new pullloadCore(coreOpts);
+      addEvent(this.wrapper, "touchstart", bind(this.onTouchStart, this));
+      addEvent(this.wrapper, "touchmove", bind(this.onTouchMove, this));
+      addEvent(this.wrapper, "touchend", bind(this.onTouchEnd, this));
+    },
+    destory: function(){
+      removeEvent(this.wrapper, "touchstart", this.onTouchStart);
+      removeEvent(this.wrapper, "touchmove", this.onTouchMove);
+      removeEvent(this.wrapper, "touchend", this.onTouchEnd);
+    },
+    onTouchStart: function(event){
+      var targetEvent = event.changedTouches[0],
+        startX = targetEvent.clientX,
+        startY = targetEvent.clientY;
+      console.info("onTouchStart");
+      this._core.onStart(startX, startY);
+    },
+    onTouchMove: function(event){
+      var targetEvent = event.changedTouches[0],
+        startX = targetEvent.clientX,
+        startY = targetEvent.clientY,
+        scrollTop = this.container.scrollTop,
+        scrollH = this.container.scrollHeight,
+        conH = this.container === document.body ? document.documentElement.clientHeight : this.container.offsetHeight;
+
+      this._core.onMove(startX, startY, scrollTop, scrollH, conH);
+    },
+    onTouchEnd: function(event){
+      var targetEvent = event.changedTouches[0],
+        startX = targetEvent.clientX,
+        startY = targetEvent.clientY,
+        scrollTop = this.container.scrollTop,
+        scrollH = this.container.scrollHeight,
+        conH = this.container === document.body ? document.documentElement.clientHeight : this.container.offsetHeight;
+
+      console.info("onTouchEnd");
+      this._core.onEnd(startX, startY, scrollTop, scrollH, conH);
+    },
+    // 拖拽的缓动公式 - easeOutSine
+    easing(distance) {
+      // t: current time, b: begInnIng value, c: change In value, d: duration
+      var t = distance;
+      var b = 0;
+      var d = screen.availHeight; // 允许拖拽的最大距离
+      var c = d / 2.5; // 提示标签最大有效拖拽距离
+
+      return c * Math.sin(t / d * (Math.PI / 2)) + b;
+    },
+    setChange: function(pullHeight, state){
+      var lbodyTop = pullHeight !== 0 ? 'translate3d(0, ' + pullHeight + 'px, 0)' : "",
+        symbolTop = pullHeight - 50 > 0 ? pullHeight - 50 : 0;
+        lSymbol = symbolTop !== 0 ? 'translate3d(0, ' + symbolTop + 'px, 0)' : "";
+      // console.info(lbodyTop)
+      this.setClassName(state);
+      this.loaderBody.style.WebkitTransform = lbodyTop;
+      this.loaderBody.style.transform = lbodyTop;
+      this.loaderSymbol.style.WebkitTransform = lSymbol;
+      this.loaderSymbol.style.transform = lSymbol;
+    },
+    setClassName: function(state){
+      this.loaderState = state;
+      this.wrapper.className = 'tloader state-' + state;
+    },
+    setEndState: function(){
+      this.setChange(0, STATS.reset);
+    },
+    setNoMoreState:function(){
+      this.loaderBtn.style.display = "block";
+      this.hasMore = false;
+    },
+    resetLoadMore: function(){
+      this.loaderBtn.style.display = "none";
+      this.hasMore = true;
+    },
+    onPullDownMove: function(startY, y){
+      event.preventDefault();
+
+      var diff = y - startY, loaderState;
+      if (diff < 0) {
+        diff = 0;
+      }
+      console.info(startY, y)
+      diff = this.easing(diff);
+      if (diff > 100) {
+        loaderState = STATS.enough;
+      } else {
+        loaderState = STATS.pulling;
+      }
+      this.setChange(diff, loaderState);
+    },
+    onPullDownRefresh: function(){
+      if (this.loaderState === STATS.pulling) {
+        this.setEndState();
+      } else {
+        this.setChange(0, STATS.refreshing);
+        this.resetLoadMore();
+        if (typeof this.config.onRefresh === "function") {
+          this.config.onRefresh(
+            bind(function(){
+              this.setChange(0, STATS.refreshed);
+              setTimeout(bind(function(){this.setChange(0, STATS.init);}, this), 1000);
+            }, this),
+            bind(function(){
+              this.setEndState();
+            }, this)
+          )
+        }
+      }
+    },
+    clearPullDownMove: function(){},
+    onPullUpMove: function(staartY, y){
+      if(!this.hasMore){
+        return false;
+      }
+      if (typeof this.config.onLoadMore === "function") {
+        this.setChange(0, STATS.loading);
+        // console.info(this.state);
+        this.config.onLoadMore(bind(function(isNoMore){
+          this.setEndState();
+          if(isNoMore){
+            this.setNoMoreState();
+          }
+        }, this));
+      }
+    },
+    onPullUpLoad: function(){}
+  }
+
+  function extendArrProps(arr, obj1, obj2){
+    var index = 0, len = arr.length;
+    for(index; index < len; index++){
+      var value = arr[index];
+      if(typeof obj2[value] !== "undefined"){
+        obj1[value] = obj2[value];
+      }
+    }
+    return obj1;
+  }
+
+  //copy obj2 props to obj1 no deepClone
+  function extend(obj1, obj2){
+    var newObj = {};
+    for(var s in obj1){
+      newObj[s] = obj1[s]
+    }
+    for(var s in obj2){
+      newObj[s] = obj2[s]
+    }
+    return newObj;
+  }
+
+  function addEvent(obj, type, fn) {
+    if (obj.attachEvent) {
+      obj['e' + type + fn] = fn;
+      obj[type + fn] = function () { obj['e' + type + fn](window.event); }
+      obj.attachEvent('on' + type, obj[type + fn]);
+    } else
+      obj.addEventListener(type, fn, false);
+  }
+  function removeEvent(obj, type, fn) {
+    if (obj.detachEvent) {
+      obj.detachEvent('on' + type, obj[type + fn]);
+      obj[type + fn] = null;
+    } else
+      obj.removeEventListener(type, fn, false);
+  }
+
+  function asArray(quasiArray, start) {
+    var result = [],
+      i = (start || 0);
+    for (; i < quasiArray.length; i++) {
+      result.push(quasiArray[i]);
+    }
+    return result;
+  }
+
+  function bind(func, context) {
+    if (arguments.length < 2 && typeof arguments[0] === "undefined") {
+        return func;
+    }
+
+    var __method = func;
+    var args = asArray(arguments, 2);
+
+    return function() {
+		var array = args.concat(asArray(arguments, 0));
+        return __method.apply(context, array);
+    };
+}
+
+  // var obj1 = {
+  //  name:1,
+  //  age:12,
+  // }
+  // var obj2 = {
+  //   name:2
+  // }
+  // console.info(extend({},obj2));
+  // console.info(extend(obj1,{}));
+  // console.info(extend(obj1,obj2));
+  // obj1.name = 3
+  window.pullload = pullload;
+})()
